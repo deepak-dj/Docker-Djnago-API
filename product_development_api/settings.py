ea6c7396556a99dd69255f898e4d092403ae29f4
deepak-dj <joshig.deepak@gmail.com>
2022-03-28 02:09:52 +0530

    updated yml file

--- product_development_api/settings.py
+++ product_development_api/settings.py
@@ -61,20 +61,20 @@ WSGI_APPLICATION = "product_development_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "django_db",
-#         "USER": "root",
-#         "PASSWORD": "root",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_db",
+        "USER": "root",
+        "PASSWORD": "root",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
